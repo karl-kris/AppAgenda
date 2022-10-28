953336f6d8204bdde37ddd673a17f1092ad8cd60
Kristian Johansson <carlkristian.johansson@gmail.com>
2022-10-28 13:16:49 +0200

    Pruebas de uso de la base de datos JDBC

--- src/appagenda/AppAgenda.java
+++ src/appagenda/AppAgenda.java
@@ -4,13 +4,16 @@
  */
 package appagenda;
 
+import entidades.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /**
  *
- * @author usu2dam
+ * @author Kristian Johansson Dougal
  */
 public class AppAgenda {
 
@@ -18,8 +21,34 @@ public class AppAgenda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EntityManagerFactory emf=Persistence.createEntityManagerFactory("AppAgendaPU");
-        EntityManager em=emf.createEntityManager();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AppAgendaPU");
+        EntityManager em = emf.createEntityManager();
+        
+        // Iniciar una transacción
+        em.getTransaction().begin();
+
+        // Modificaciones
+        Provincia provinciaCadiz = new Provincia();
+        provinciaCadiz.setNombre("Cádiz");
+        Provincia provinciaSevilla = new Provincia();
+        provinciaSevilla.setNombre("Sevilla");
+        
+        em.persist(provinciaCadiz);
+        em.persist(provinciaSevilla);
+        
+        // Volcado a la base de datos
+        em.getTransaction().commit();
+
+        // Cancelar todas las operaciones
+        //em.getTransaction().rollback();
+        
+        em.close();
+        emf.close();
+        try{
+            DriverManager.getConnection("jdbc:derby:BDAgenda;shutdown=true");
+        } catch (SQLException ex){
+        }
+
     }
     
 }
